0eb2c8d1dd6722e71adee9620b78656f112b5e3d
abelhubArchi <bolevel12@gmail.com>
2023-10-11 00:04:00 -0400

    esta

--- src/routes/js/axios.js
+++ src/routes/js/axios.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 var local = 'http://192.168.0.18:3000';
-var web = 'https://abogacy-519e5409953a.herokuapp.com';
+var web = 'https://abogacy.onrender.com/';
 
-axios.defaults.baseURL = local;
+axios.defaults.baseURL = web;
 
 var IA;
 let chatMessages;
 
 export const makeRequest = async(method, url, data) => {
     const config = {
-      baseURL: local,
+      baseURL: web,
       headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': 'https://abogacyio.netlify.app'
